011c10ce1db68ff5466b4c0fb73891a84342f1ef
priya <priya4893@gmail.com>
2024-12-07 18:36:48 +0800

    dummy login without auth

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,3 +1,6 @@
+import { Box, Button, FormLabel, Grid2 } from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
@@ -5,11 +8,56 @@ const Login = () => {
   const handleClick = () => {
     navigate("/dashboard");
   };
+  const formik = useFormik({
+    initialValues: {
+      cust_id: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      amount: Yup.number().required('Amount is required').positive('Amount must be positive'),
+      remarks: Yup.string(),
+    }),
+    onSubmit: () => {
+      handleClick();
+    },
+  });
 
-  return (
-    <div>
-      <button onClick={() => handleClick()}>Go to dashboard</button>
-    </div>
+  return (<>
+    <h3>ABC Mortgage Banking</h3>
+    <form className="form-container" onSubmit={formik.handleSubmit}>
+      <Box mb={1} p={0}>
+        <Grid2 container>
+          <Grid2 size={6}>
+            <FormLabel>Customer ID</FormLabel>
+          </Grid2>
+          <Grid2 size={6}>
+            <input
+              type="text"
+              name="cust_id"
+              className='form-input'
+              onChange={formik.handleChange}
+              value={formik.values.cust_id}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <FormLabel>Password</FormLabel>
+          </Grid2>
+          <Grid2 size={6}>
+          <input
+            type="password"
+            name="password"
+            className='form-input'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.password} />
+          </Grid2>
+          <Box className="actions">
+            <Button variant="outlined" onClick={handleClick} className='submit' type="submit">LOGIN</Button>
+          </Box>
+        </Grid2>
+      </Box>
+    </form>
+    </>
   );
 };
 
