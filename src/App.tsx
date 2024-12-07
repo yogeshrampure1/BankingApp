9a21545eea8cc7ce7a6d15034e113649adbe498b
Meenakshi Sundaram M <sundar.meenakshi@saladstop.com>
2024-12-07 13:45:26 +0530

    reformatting prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Login from "./components/auth/Login";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import PrivateRoute from "./routes/PrivateRoute";
-import Header from "./components/header/Header"
+import Header from "./components/header/Header";
 import "./App.css";
 
-
-
 function App() {
-  const [isAuthenticated,setIsAuthenticated] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
   return (
     <Router>
-    <Routes>
-    {/* <Route path="/login" element={<Login onLogin={() => setIsAuthenticated(true)} />} /> */}
-      <Route path="/login" element={<Login  />} />
-      <Route path="/" element={<Login  />} />
-    
-      <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute isAuthenticated={isAuthenticated}>
-            <Header userName='Sundar'/>
-          </PrivateRoute>
-        }
-      />
-       <Route
-        path="/fundtransfer"
-        element={
-          <PrivateRoute isAuthenticated={isAuthenticated}>
-            'fundtransfer'
-          </PrivateRoute>
-        }
-      />
-    </Routes>
-  </Router>
+      <Routes>
+        {/* <Route path="/login" element={<Login onLogin={() => setIsAuthenticated(true)} />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              <Header userName="Sundar" />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/fundtransfer"
+          element={
+            <PrivateRoute isAuthenticated={isAuthenticated}>
+              'fundtransfer'
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,10 +1,13 @@
-import React, { ReactNode } from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
 interface PrivateRouteProps {
-    children: ReactNode;
-    isAuthenticated: boolean;
-  }
-const PrivateRoute:React.FC<PrivateRouteProps> = ({ children, isAuthenticated }) => {
+  children: ReactNode;
+  isAuthenticated: boolean;
+}
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  children,
+  isAuthenticated,
+}) => {
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
